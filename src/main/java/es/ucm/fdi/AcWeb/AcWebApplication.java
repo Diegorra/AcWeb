e16a75e6aec06cdf0cdc26ab9caa9fb842a880fc
Diego <56647811+Diegorra@users.noreply.github.com>
2023-01-27 17:10:47 +0100

    Initial commit

---
+++ src/main/java/es/ucm/fdi/AcWeb/AcWebApplication.java
@@ -0,0 +1,13 @@
+package es.ucm.fdi.AcWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/ucm/fdi/AcWeb/AcWebApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ucm.fdi.AcWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
