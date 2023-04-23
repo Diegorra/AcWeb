3b52d4a92800b7d1ae44b42ff98a9161da0ad9cc
Diego <56647811+Diegorra@users.noreply.github.com>
2023-04-23 18:06:57 +0200

    Extract sources from Campus Virtual, download clean sources, code comparison and other fixes

---
+++ src/main/java/es/ucm/fdi/acweb/ZipFileExtractor.java
@@ -0,0 +1,73 @@
+package es.ucm.fdi.acweb;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Enumeration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class ZipFileExtractor {
+
+    public void extractZip(MultipartFile zipFile, Path outputDirectory) throws IOException {
+        File tempFile = File.createTempFile("zip", null);
+        zipFile.transferTo(tempFile);
+        extractZip(tempFile, outputDirectory);
+        tempFile.delete();
+    }
+
+    private void extractZip(File zipFile, Path outputDirectory) throws IOException {
+        // Crea un patrón de expresión regular para capturar un código numérico de 7 dígitos
+        Pattern pattern = Pattern.compile("(\\d{7})");
+        try (ZipFile zf = new ZipFile(zipFile, Charset.forName("CP437"))) {
+            Enumeration<? extends ZipEntry> entries = zf.entries();
+            while (entries.hasMoreElements()) {
+                ZipEntry zipEntry = entries.nextElement();
+                Path filePath = outputDirectory.resolve(zipEntry.getName());
+                if (zipEntry.isDirectory()) {
+                    Files.createDirectories(filePath);
+                } else {
+                    Files.createDirectories(filePath.getParent());
+                    Files.copy(zf.getInputStream(zipEntry), filePath);
+
+                    if (isZipFile(filePath)) {
+                        // Extrae el código numérico de 7 dígitos del nombre del archivo
+                        Matcher matcher = pattern.matcher(zipEntry.getName());
+                        String subfolderName;
+                        if (matcher.find()) {
+                            subfolderName = matcher.group(1);
+                        } else { //si no encuentra el código del Campus
+                            subfolderName = zipEntry.getName().replaceAll("\\.zip$", "");
+                        }
+
+                        // Crea una carpeta con el nombre subfolderName
+                        Path subfolder = outputDirectory.resolve(subfolderName);
+                        Files.createDirectories(subfolder);
+                        // Extrae el contenido del archivo ZIP en la carpeta creada
+                        extractZip(filePath.toFile(), subfolder);
+                        // Elimina el archivo ZIP
+                        Files.delete(filePath);
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean isZipFile(Path file) {
+        try (FileInputStream fis = new FileInputStream(file.toFile())) {
+            byte[] signature = new byte[4];
+            if (fis.read(signature) != 4) {
+                return false;
+            }
+            int sig = ((signature[0] & 0xFF) << 24) | ((signature[1] & 0xFF) << 16) | ((signature[2] & 0xFF) << 8) | (signature[3] & 0xFF);
+            return sig == 0x504B0304;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+}
